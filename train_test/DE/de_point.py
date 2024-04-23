c840e76ae51a66b6c894846afdb81e543e365773
“luca-pernigo” <luca.pernigo@usi.ch>
2024-04-23 15:44:30 +0200

    point metrics

---
+++ train_test/DE/de_point.py
@@ -0,0 +1,20 @@
+# script for point prediction metrics on median R2, RMSE, MAE
+
+import numpy as np
+import pandas as pd
+
+
+from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
+
+test=pd.read_csv("Data/DE/2021/clean/de_test.csv")
+y_test=test["Load"]
+
+df_pred=pd.read_csv("Data/DE/2021/clean/model_prediction.csv")
+
+pred=df_pred["0.5"]
+
+print("R2 score: ", r2_score(y_test, pred))
+
+print("Normalized RMSE: ", np.sqrt(mean_squared_error(y_test, pred))/np.mean(y_test))
+
+print("Normalized MAE: ", mean_absolute_error(y_test, pred)/np.mean(y_test))
