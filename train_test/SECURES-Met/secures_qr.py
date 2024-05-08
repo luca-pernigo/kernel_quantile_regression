57439d9ee9972bcc998bc784e8ac03d8e1fec4c8
“luca-pernigo” <luca.pernigo@usi.ch>
2024-05-08 09:20:48 +0200

    train test experiment kernel comparison

---
+++ train_test/SECURES-Met/secures_qr.py
@@ -0,0 +1,74 @@
+# script to train models on SECURES-Met data
+import numpy as np
+import pandas as pd
+import pickle
+
+from sklearn.metrics import make_scorer
+from sklearn.experimental import enable_halving_search_cv  # noqa
+from sklearn.model_selection import HalvingRandomSearchCV
+
+from sklearn.metrics import mean_pinball_loss
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+import sys
+from tqdm import tqdm
+
+sys.path.append("/Users/luca/Desktop/kernel_quantile_regression/")
+from src.kernel_quantile_regression.kqr import KQR
+
+
+country="DE"
+ktype="linear"
+# load train data
+df=pd.read_csv(f"/Users/luca/Desktop/kernel_quantile_regression/Data/SECURES-Met/{country}/clean/train/df.csv")
+
+# quantiles
+# quantiles = [i/100 for i in range(1,100)]
+# quantiles = [0.05, 0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9, 0.95]
+quantiles = [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9]
+
+
+# for the experiment with just temperature and wind_speed exp=""
+# X y
+X_train=df[["Direct_irradiation","Global_radiation","Hydro_reservoir","Hydro_river","Temperature","Wind_potential"]]
+y_train=df["Load"]
+
+# scale
+scaler = StandardScaler()
+
+X_train_scaled = scaler.fit_transform(X_train)
+
+# kernel quantile regression
+qr_krn_models=[]
+y_test_pred_qr_krn=[]
+
+# gamma=[1e-1,1e-2,0.5,1, 5, 10, 20]
+# gamma=[1/1e-1,1/1e-2,1/0.5,1, 1/5, 1/10, 1/20]
+param_grid_krn = dict(
+C=[1e-1,1e-2,1, 5, 10,1e2,1e4]
+)
+
+neg_mean_pinball_loss_scorer_05 = make_scorer(
+    mean_pinball_loss,
+    alpha=0.5,
+    greater_is_better=False,
+    )
+
+krn_blueprint=KQR(alpha=0.5, kernel_type=ktype)
+best_hyperparameters_krn=HalvingRandomSearchCV(
+        krn_blueprint,
+        param_grid_krn,
+        scoring=neg_mean_pinball_loss_scorer_05,
+        n_jobs=2,
+        random_state=0,
+    ).fit(X_train, y_train).best_params_
+
+# train
+for i,q in enumerate(tqdm(quantiles)):
+    
+    # fit data for specific quantile
+    qr_krn_models+=[KQR(alpha=q, **best_hyperparameters_krn, kernel_type=ktype).fit(X_train_scaled, y_train)]
+
+    # save models to pickle
+    pickle.dump(qr_krn_models[i], open(f'/Users/luca/Desktop/kernel_quantile_regression/train_test/SECURES-Met/{country}/{ktype}/krn_qr_{q}.pkl', 'wb'))
+    
---
+++ train_test/SECURES-Met/secures_qr_test.py
@@ -0,0 +1,78 @@
+import matplotlib.pyplot as plt
+from matplotlib import colormaps
+
+import numpy as np
+
+import pandas as pd
+import pickle
+
+import os
+
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import mean_pinball_loss
+from sklearn.preprocessing import StandardScaler
+import sys
+
+from tqdm import tqdm
+
+wd = os.path.dirname(os.path.abspath(__file__))
+project_directory = os.path.abspath(os.path.join(wd, '..', '..'))
+sys.path.append(project_directory)
+
+from utils import miscellaneous
+
+
+country="DE"
+ktype="gaussian_rbf"
+
+# load train test data
+
+train=pd.read_csv(f"/Users/luca/Desktop/kernel_quantile_regression/Data/SECURES-Met/{country}/clean/train/df.csv")
+test=pd.read_csv(f"/Users/luca/Desktop/kernel_quantile_regression/Data/SECURES-Met/{country}/clean/test/df.csv")
+
+
+# X y
+X_train=train[["Direct_irradiation","Global_radiation","Hydro_reservoir","Hydro_river","Temperature","Wind_potential"]]
+X_test=test[["Direct_irradiation","Global_radiation","Hydro_reservoir","Hydro_river","Temperature","Wind_potential"]]
+
+y_train=train["Load"]
+y_test=test["Load"]
+
+quantiles = [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9]
+
+# scale data
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+# predict df
+df_predict=pd.DataFrame(columns=[f"{q}" for q in quantiles])
+
+for i,q in enumerate(quantiles):
+    # load model
+    krn_q=pickle.load(open(f'/Users/luca/Desktop/kernel_quantile_regression/train_test/SECURES-Met/{country}/{ktype}/krn_qr_{q}.pkl', 'rb'))
+    # predict
+    y_predict_q=krn_q.predict(X_test_scaled)
+    # print(mean_pinball_loss(y_test,y_predict_q, alpha=q)/np.mean(y_test))
+    # put in predict df
+    df_predict[f"{q}"]=pd.Series(y_predict_q)
+
+# save predictions to csv
+df_predict.to_csv(f"/Users/luca/Desktop/kernel_quantile_regression/Data/SECURES-Met/{country}/clean/model_prediction_{ktype}.csv", index=False)
+
+
+# compute pinball loss
+pinball_tot=0
+for i,q in enumerate(quantiles):
+    # normalized pinball loss
+    pinball_q=mean_pinball_loss(y_test,df_predict[f"{q}"], alpha=q)/np.mean(y_test)
+    # print(f"pinball loss quantile {q}: ", pinball_q)
+    print(f"{q}  &  {pinball_q}  \\\\ ")
+    pinball_tot+=pinball_q
+
+
+ans=pinball_tot/len(quantiles)
+print("total quantile loss: ", ans)
+
+
+
