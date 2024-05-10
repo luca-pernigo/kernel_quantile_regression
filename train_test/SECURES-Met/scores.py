96168ed87d4100166c908382d7e3ed4795ed241f
“luca-pernigo” <luca.pernigo@usi.ch>
2024-05-10 10:18:44 +0200

    scores update

--- train_test/SECURES-Met/scores.py
+++ train_test/SECURES-Met/scores.py
@@ -6,12 +6,12 @@ from sklearn.metrics import mean_pinball_loss
 # script for storing scores SECURES-Met data
 
 quantiles=[0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9]
-country="CH"
+country="AT"
 
 test=pd.read_csv(f"/Users/luca/Desktop/kernel_quantile_regression/Data/SECURES-Met/{country}/clean/test/df.csv")
 y_test=test["Load"]
 
-kernels=["gaussian_rbf", "laplacian", "matern_0.5", "matern_1.5", "matern_2.5", "linear", "periodic", "polynomial", "sigmoid", "cosine"]
+kernels=["gaussian_rbf", "laplacian", "matern_1.5", "matern_2.5", "linear", "periodic", "polynomial", "sigmoid", "cosine"]
 
 try:
     pinball_scores=pd.read_csv(f"train_test/SECURES-Met/{country}/scores.csv")
